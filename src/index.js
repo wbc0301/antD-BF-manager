84da8f2a87d4c21370375f042fe21c7da0363a4b
wbc <417780894@qq.com>
2020-02-03 17:28:26 +0800

    wbc2 test

--- src/index.js
+++ src/index.js
@@ -12,3 +12,5 @@ ReactDOM.render(<Admin />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// wbc2 test 
