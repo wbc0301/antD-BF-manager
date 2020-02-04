2fb5ab6dede017bab69b0f99e4866f1b43113589
wbc <417780894@qq.com>
2020-02-04 20:25:26 +0800

    button

--- src/pages/ui/buttons/index.js
+++ src/pages/ui/buttons/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Card, Button} from 'antd';
+import './../ui.less';
 
 export default class Buttons extends React.Component{
   render() {
     return (
-      <div>This is buttons page.</div>
+      <div>
+        <Card title="基础按钮" className="card-wrap"> 
+          <Button type="primary">Imooc</Button>
+          <Button>Imooc</Button>
+          <Button type="dashed">Imooc</Button>
+        </Card>
+
+        <Card title="图形按钮"  className="card-wrap"> 
+          <Button type="primary" icon="download" >下载</Button>
+          <Button type="primary" icon="delete" >删除</Button>
+        </Card>
+
+      </div>
     )
   }
 }
